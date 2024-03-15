7195e97a673b8240ac4b4b801dfa95ce8a2d1285
Fred L <fphenix@hotmail.com>
2024-03-15 11:01:43 +0100

    "Above" bug fixed

--- scripts/hoh_rooms.py
+++ scripts/hoh_rooms.py
@@ -29,7 +29,7 @@ def fwrite(file, label, str1, str2 = ""):
 	file.write(label + ": " + str(str1) + str2 + "\n")
 
 #---------------------------------------------------------------------------------------------------
-def unpack_Room(fw, roomId):
+def unpack_Room(fw, roomId, csvList):
 	fwrite(fw, "#", "|" * 80)
 
 	fwrite(fw, "Room ID", roomId)
@@ -43,6 +43,11 @@ def unpack_Room(fw, roomId):
 	fwrite(fw, "World", worldId, worldIds[worldId])
 	doorId, r = extract(3, r)
 	fwrite(fw, "Door SpriteId", doorId)
+
+	row, col, elevation = (int(roomId[0], 16), int(roomId[1], 16), int(roomId[2], 16))
+	csvList[row][col][elevation] = world1letter[worldId] + ":" + str(roomId)
+	csvList[row][col][elevation] += " : Head"  if roomId == "8A4" else ""
+	csvList[row][col][elevation] += " : Heels" if roomId == "894" else ""
 
 	doorData = []
 	for i in range(4):
@@ -85,6 +90,7 @@ def unpack_Room(fw, roomId):
 				if objid == 0:
 					if roomId in Teleport.keys():
 						fwrite(fw, "\tTeleport Destination", Teleport[roomId])
+						csvList[row][col][elevation] += " (T:" + Teleport[roomId] + ")"
 
 				if flags10 < 0:
 					flags10, r = extract(2, r)
@@ -193,13 +199,13 @@ def print_room(data):
 		SEcell[2] = SEcell[5] = "-"
 		SEcell[3] = SEcell[4] = data["Door2"][1]
 	
-	# draw the NE wall/door
+	# print Up room code (if any)
 	res = "\nRoomID = " + data["RoomID"] + " (" + data["World"] + ")\n"
 	if data["Door1"][1] != "" or data["Door1"][0] == "":
 		res += (" " * 5)
 		res += (" " * ((longuest+2) * 3)) + "   " + next_room(data["RoomID"], [1, 0, 0])
 	res += "\n"
-
+	# draw the NE wall/door
 	res += (" " * 5)
 	if data["Door1"][0] == "W" and data["Door1"][1] == "":
 		res +=  wall * ((longuest+2) * 8) 
@@ -207,17 +213,19 @@ def print_room(data):
 		res += (wall * ((longuest+2) * 3)) + " [ " + (data["Door1"][1] * (((longuest+2) * 2) - 6)) + " ] " + (wall * ((longuest+2) * 3))
 	else:
 		res += nowall * ((longuest+2) * 8)
-	#if room above exists and has no floor (room-data string has floor id in character [9] in bits [2:1] (mod 8) and 7 means "no floor")
-	nextroom = next_room(data["RoomID"], [0, 0, -1])
-	if (Room_list1.get(nextroom) is not None):
-		if ((int(Room_list1[nextroom][9], 16) % 8) == 7):
-			res += " UP: " + nextroom
+	#if room above exists and has no floor (room-data string has floor id in character [9] bits [3:1] ((value >> 1) mod 8) == 7 means "no floor")
+	aboveroom = next_room(data["RoomID"], [0, 0, -1])
+	if (Room_list1.get(aboveroom) is not None):
+		if (((int(Room_list1[aboveroom][9], 16) // 2) % 8) == 7):
+			res += " Above: " + aboveroom
 	res += "\n"
 
 	#draw the cells
 	for row in range(7, -1, -1):
+		#left room id if any
 		line = next_room(data["RoomID"], [0, 1, 0]) if ((data["Door0"][1] != "" or data["Door0"][0] == "") and row == 4) else "   "
 		line += " " + NWcell[row]
+		#cells in current room
 		for col in range(7, -1, -1):		
 			k = f"r{row}c{col}"
 			line += ws
@@ -225,6 +233,7 @@ def print_room(data):
 			line += (data[k] * longuest) if data[k] == inex else ""
 			line += (floor * longuest) if (len(data[k]) == 0) else "" 
 			line += we
+		#right room if any
 		res += line + SEcell[row]
 		res += " " + next_room(data["RoomID"], [0, -1, 0]) if ((data["Door2"][1] != "" or data["Door2"][0] == "") and row == 3) else ""
 		res += "\n"
@@ -237,9 +246,11 @@ def print_room(data):
 		res += (wall * ((longuest+2) * 3)) + " [ " + (data["Door3"][1] * (((longuest+2) * 2) - 6)) + " ] " + (wall * ((longuest+2) * 3))
 	else:
 		res += nowall * ((longuest+2) * 8)
+	# no floor, so add where we end up by falling down
 	if (floor == "."):
-		res += " DW: " + next_room(data["RoomID"], [0, 0, 1])
+		res += " Below: " + next_room(data["RoomID"], [0, 0, 1])
 	res += "\n"
+	# room code of Down door (if any)
 	if data["Door3"][1] != "" or data["Door3"][0] == "":
 		res += (" " * 5)
 		res += (" " * ((longuest+2) * 3)) + "   " + next_room(data["RoomID"], [-1, 0, 0]) + "\n "
@@ -248,6 +259,9 @@ def print_room(data):
 
 #---------------------------------------------------------------------------------------------------
 def next_room(room, displacement):
+	for i in range(3):
+		if displacement[i] < 0:
+			displacement[i] += 16
 	u = (int(room[0], 16) + displacement[0]) % 16
 	v = (int(room[1], 16) + displacement[1]) % 16
 	z = (int(room[2], 16) + displacement[2]) % 16
@@ -269,10 +283,27 @@ def adduvz(orig, offset):
 
 #---------------------------------------------------------------------------------------------------
 
-with open(join(cwd, "hoh_rooms.out.txt"), 'w') as fw:
-	for roomId in Room_list1.keys():
-		#roomId = "8A4" #"E3C"
-		unpack_Room(fw, roomId)
-		#break
+with open(join(cwd, "hoh_rooms.csv"), 'w') as fwcsv:
+	rows, cols, elevation = (16, 16, 16)
+	csvList = [[["" for _ in range(elevation)] for _ in range(cols)] for _ in range(rows)]
+	with open(join(cwd, "hoh_rooms.out.txt"), 'w') as fw:
+		for roomId in Room_list1.keys():
+			#roomId = "8A4" #"E3C"
+			unpack_Room(fw, roomId, csvList)
+			#break
+
+	fwcsv.write(",") # topleft empty cell
+	for col in range(15, -1, -1):
+		fwcsv.write(hex(col) + ("," if col > 0 else "\n")) # top row "col names"
+
+	for row in range(15, -1, -1):
+		fwcsv.write(hex(row) + ",")	# left col "row names"
+		for col in range(15, -1, -1):
+			tmpstr = '"'
+			for elevation in range(16):
+				addstr = csvList[row][col][elevation]
+				tmpstr += ("\n" if (addstr != "" and tmpstr != "") else "") + addstr
+			tmpstr += '"'
+			fwcsv.write(tmpstr + ("," if col > 0 else "\n"))
 
 raise SystemExit
